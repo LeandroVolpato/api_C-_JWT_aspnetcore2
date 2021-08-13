74c26c6d15bb19f1b6fb94ac4ef6d9b332433765
LeandroVolpato <87440779+LeandroVolpato@users.noreply.github.com>
2021-08-13 15:14:34 -0300

    Add files via upload

---
+++ api_C#_JWT_aspnetcore2.2/APIProdutos/Controllers/ProdutosController.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authorization;
+using APIProdutos.Business;
+using APIProdutos.Models;
+
+namespace APIProdutos.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [Authorize("Bearer")]
+    public class ProdutosController : ControllerBase
+    {
+        private ProdutoService _service;
+
+        public ProdutosController(ProdutoService service)
+        {
+            _service = service;
+        }
+
+        //get para listar produtos
+        [HttpGet]
+        public IEnumerable<Produto> Get()
+        {
+            return _service.ListarTodos();
+        }
+
+        //get para listar um produto pelo codigo de barras
+        [HttpGet("{codigoBarras}")]
+        public ActionResult<Produto> Get(string codigoBarras)
+        {
+            var produto = _service.Obter(codigoBarras);
+            if (produto != null)
+                return produto;
+            else
+                return NotFound();
+        }
+
+        //post para incluir produto
+        [HttpPost]
+        public Resultado Post([FromBody]Produto produto)
+        {
+            return _service.Incluir(produto);
+        }
+
+        [HttpPut]
+        public Resultado Put([FromBody]Produto produto)
+        {
+            return _service.Atualizar(produto);
+        }
+
+        [HttpDelete("{codigoBarras}")]
+        public Resultado Delete(string codigoBarras)
+        {
+            return _service.Excluir(codigoBarras);
+        }
+    }
+}
---
+++ api_C#_JWT_aspnetcore2.2/APIProdutos/Models/Produto.cs
@@ -0,0 +1,24 @@
+﻿namespace APIProdutos.Models
+{
+
+    //models para as variaveis do produto
+
+    public class Produto
+    {
+        private string _codigoBarras;
+        public string CodigoBarras
+        {
+            get => _codigoBarras;
+            set => _codigoBarras = value?.Trim().ToUpper();
+        }
+
+        private string _nome;
+        public string Nome
+        {
+            get => _nome;
+            set => _nome = value?.Trim();
+        }
+
+        public double Preco { get; set; }
+    }
+}
